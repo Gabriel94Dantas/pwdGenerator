ca6fdbacf3b1a8b8fb0d5eeb11e9ca940c2fdaad
Gabriel Dantas <gabriel.araujo.dantas@gmail.com>
2023-03-01 10:17:32 -0300

    My first commit, project password Generator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,66 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pwdGenerator';
+  
+  title = 'Password Generator by Dantas';
+  password = '';
+  useLetters = false;
+  useNumbers = false;
+  useSymbols = false;
+  passwordLength = 0;
+
+
+  onButtonClick(){
+    const numbers = "1234567890";
+    const letters = "abcdefghijklmnopqrstuvxwyz";
+    const symbols = "!@#$%^&*(){}[]?><,.";
+    this.password = '';
+
+    let validChars = '';
+
+    if(this.useLetters){
+      validChars = validChars + letters;
+    }
+
+    if(this.useNumbers){
+      validChars = validChars + numbers;
+    }
+
+    if(this.useSymbols){
+      validChars = validChars + symbols;
+    }
+
+    for(let i = 0; i < this.passwordLength; i++){
+      const index = Math.floor(Math.random() * validChars.length);
+      this.password = this.password + validChars[index];
+    }
+
+  }
+
+  onChangeLetters(){
+    this.useLetters = !this.useLetters;
+    return this.useLetters;
+  }
+
+  onChangeNumbers(){
+    this.useNumbers = !this.useNumbers;
+    return this.useNumbers;
+  }
+
+  onChangeSymbols(){
+    this.useSymbols = !this.useSymbols;
+    return this.useSymbols;
+  }
+
+  onChangeLength(event : any){
+    const parsedLength = parseInt(event.target.value);
+    if(!isNaN(parsedLength)){
+      this.passwordLength = parsedLength;
+    }
+  }
+
+  isDisabled(){
+    return !(this.passwordLength && (this.useNumbers || this.useLetters || this.useSymbols));
+  }
+
 }
